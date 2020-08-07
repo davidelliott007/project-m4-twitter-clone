572b682bfb21dec4938d588493df0ab050ce7ed4
David Elliott <dave.elliott@gmail.com>
2020-08-07 17:43:25 -0400

    working on profiles

---
+++ client/src/MeowListItem.js
@@ -0,0 +1,164 @@
+import React from "react";
+import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import {
+  FiArrowLeft,
+  FiMessageCircle,
+  FiRepeat,
+  FiHeart,
+  FiUpload,
+} from "react-icons/fi";
+
+import Sidebar from "./SideBar";
+
+function MeowListItem(props) {
+  const itemId = useParams().tweetid;
+
+  const [tweetID, settweetID] = React.useState(itemId);
+  const [author, setAuthor] = React.useState();
+  const [authorImg, setAuthorImg] = React.useState();
+  const [handle, setHandle] = React.useState();
+  const [tweetImg, settweetImg] = React.useState();
+  const [tweetStatus, setTweetStatus] = React.useState();
+  const [tweetTimeStamp, setTweetTimeStamp] = React.useState();
+
+  React.useEffect(() => {
+    // const apiUrl = "http://localhost:3000/api/me/profile";
+    // fetch(apiUrl)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //   });
+    // const apiUrl = "http://localhost:3000/api/tweet/" + itemId;
+    // fetch(apiUrl)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log("got tweet data MeowListItems");
+    //     console.log(data.tweet.media);
+    //     setAuthor(data.tweet.author.displayName);
+    //     setHandle(data.tweet.author.handle);
+    //     settweetImg(data.tweet.media[0].url);
+    //     setTweetStatus(data.tweet.status);
+    //     setAuthorImg(data.tweet.author.avatarSrc);
+    //     let date = new Date(data.tweet.timestamp);
+    //     let date_string = date.toDateString();
+    //     let time_string = date.toLocaleTimeString("en-US");
+    //     let full = time_string + " - " + date_string;
+    //     setTweetTimeStamp(full);
+    //   });
+  }, []);
+
+  return (
+    <TweetSection>
+      <NavigationArrow>
+        <FiArrowLeft></FiArrowLeft> <MeowTitle>Meow</MeowTitle>
+      </NavigationArrow>
+      <Seperator />
+
+      <Author>
+        <AuthorImg src={authorImg} alt="authorImg Image" />
+        <AuthorNameHandle>
+          <Name>{author}</Name>
+          <Handle>@{handle}</Handle>
+        </AuthorNameHandle>
+      </Author>
+      <TweetStatus>{tweetStatus}</TweetStatus>
+      <TweetImg src={tweetImg} alt="Tweet Image" />
+      <DateSection>{tweetTimeStamp}</DateSection>
+      <Seperator />
+      <TweetButtons>
+        <FiMessageCircle />
+        <FiRepeat />
+        <FiHeart></FiHeart>
+        <FiUpload></FiUpload>
+      </TweetButtons>
+    </TweetSection>
+  );
+}
+
+const MeowTitle = styled.div`
+  padding-left: 10px;
+`;
+
+const NavigationArrow = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 20px;
+  font-weight: bold;
+  width: 90px;
+`;
+
+const Seperator = styled.div`
+  margin-top: 10px;
+  border-top: 1px solid #bbb;
+  padding-bottom: 10px;
+`;
+
+const TweetButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  opacity: 0.7;
+  justify-content: space-around;
+  /* padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 10px; */
+`;
+
+const Name = styled.div`
+  font-weight: bold;
+`;
+
+const Handle = styled.div`
+  color: grey;
+`;
+
+const Author = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const AuthorNameHandle = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 10px;
+`;
+
+const AuthorImg = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 40px;
+`;
+
+const TweetImg = styled.img`
+  width: 400px;
+  border-radius: 10px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 30px;
+`;
+
+const TweetStatus = styled.div`
+  font-weight: bold;
+  font-size: 20px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
+
+const TweetSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #bbb;
+  border-radius: 2px;
+  padding: 15px;
+`;
+const DateSection = styled.div`
+  color: gray;
+  font-size: 13px;
+  padding-top: 10px;
+`;
+
+export default MeowListItem;
--- client/src/Profile.js
+++ client/src/Profile.js
@@ -1,12 +1,48 @@
 import React from "react";
 import styled from "styled-components";
 import Sidebar from "./SideBar";
+import MeowListItem from "./MeowListItem";
 
 function Profile(props) {
+  const [tweetsFromUser, setTweetsFromUser] = React.useState([]);
+
+  function getFeed(feedhandle) {
+    console.log("get Feed");
+    const apiUrl = "http://localhost:3000/api/" + feedhandle + "/feed";
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        return data.profile.handle;
+      });
+  }
+  function getCurrentUser() {
+    const apiUrl = "http://localhost:3000/api/me/profile";
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.profile.handle);
+        return data.profile.handle;
+      })
+      .then((handle) => {
+        getFeed(handle);
+        //console.log(handle);
+      });
+  }
+
+  React.useEffect(() => {
+    getCurrentUser();
+  }, []);
+
+  // /api/:handle/feed
+
   return (
     <Wrapper>
       <Sidebar></Sidebar>
-      <MainSection>Profile Main Secion</MainSection>
+      <MainSection>
+        what
+        {tweetsFromUser.map(<MeowListItem></MeowListItem>)}
+      </MainSection>
     </Wrapper>
   );
 }
